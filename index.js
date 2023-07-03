6c285385a51a2036e36057f03bade09b7f90c1f7
Santajit Patra <business.santajitpatra@gmail.com>
2023-07-04 03:43:31 +0530

    Query send to url
    Query send through URL by using npm i url

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 const http = require('http');
 const fs = require('fs');
+const url = require("url");
+
 
 
 const server = http.createServer((req, res) => {
+    if (req.url ==="/favicon.ico") return res.end();
     const log = `${Date.now()}: ${req.url} New request received\n`;
+    const myUrl = url.parse(req.url, true)
+console.log(myUrl)
+
     // console.log(req)
     fs,fs.appendFile("log.txt", log, (err, log) => {
 // res.end("Hello, Server is listening on");
-switch (req.url) {
+switch (myUrl.pathname) {
   case "/":
     res.end("HomePage");
     break;
   case "/about":
-    res.end("AboutPage");
+    const username = myUrl.query.user;
+    res.end(`HI ${username}`);
     break;
-  default:    res.end("404");
+  case "/search":
+    const search = myUrl.query.search_query;
+    res.end("here is "+ search);
+    break;
+  default:
+    res.end("404");
 
     break;
 }
