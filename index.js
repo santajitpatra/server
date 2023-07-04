91077c3c480a16948526a3510e3fbe88f845d4eb
Santajit Patra <business.santajitpatra@gmail.com>
2023-07-04 14:01:39 +0530

    http method
    http methods like GET, POST, PUT, PATCH,DELETE

--- index.js
+++ index.js
@@ -1,39 +1,44 @@
-const http = require('http');
-const fs = require('fs');
+const http = require("http");
+const fs = require("fs");
 const url = require("url");
 
-
-
 const server = http.createServer((req, res) => {
-    if (req.url ==="/favicon.ico") return res.end();
-    const log = `${Date.now()}: ${req.url} New request received\n`;
-    const myUrl = url.parse(req.url, true)
-console.log(myUrl)
+  if (req.url === "/favicon.ico") return res.end();
+  const log = `${Date.now()}: ${req.method} ${req.url} New request received\n`;
+  const myUrl = url.parse(req.url, true);
+  // console.log(myUrl)
 
-    // console.log(req)
-    fs,fs.appendFile("log.txt", log, (err, log) => {
-// res.end("Hello, Server is listening on");
-switch (myUrl.pathname) {
-  case "/":
-    res.end("HomePage");
-    break;
-  case "/about":
-    const username = myUrl.query.user;
-    res.end(`HI ${username}`);
-    break;
-  case "/search":
-    const search = myUrl.query.search_query;
-    res.end("here is "+ search);
-    break;
-  default:
-    res.end("404");
+  // console.log(req)
+  fs,
+    fs.appendFile("log.txt", log, (err, log) => {
+      // res.end("Hello, Server is listening on");
+      switch (myUrl.pathname) {
+        case "/":
+          if (req.method === "GET") res.end("HomePage");
+          break;
+        case "/about":
+          const username = myUrl.query.user;
+          res.end(`HI ${username}`);
+          break;
+        case "/search":
+          const search = myUrl.query.search_query;
+          res.end("here is " + search);
+          break;
+        case "/signup":
+          if (req.method === "GET") res.end("here is a signup form");
+          else if (req.method === "POST") {
+            //DB Query
+            res.end("Success");
+          }
+          break;
+        default:
+          res.end("404");
 
-    break;
-}
-    })
-})
-
+          break;
+      }
+    });
+});
 
 server.listen(8000, () => {
-    console.log("Server Starts")
+  console.log("Server Starts");
 });
